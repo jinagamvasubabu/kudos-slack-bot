77fe4161af374b3504c8bf7bfaef88cfc6c2be61
Vasubabu Jinagam <jinagamvasubabu@gmail.com>
2025-05-12 19:46:39 +0530

    fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sheets_integration import sheets
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
